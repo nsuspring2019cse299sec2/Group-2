c5468bc78f34e6ef6b05cc8667bb124e301c181b
Borshon96 <sajid.borshon@northsouth.edu>
2019-04-05 10:48:25 +0600

    basic dynamic routing for app.js

--- PROJECT CODE/app.js
+++ PROJECT CODE/app.js
@@ -1,22 +1,51 @@
-var experss = require("express");
-var app = experss();
+var express = require("express");
+var app = express();
 
-// "/" -> "HI THERE!"
 app.get("/", function(req, res){
-    console.log("someone requested for the home");
-    res.send("HI THERE!");
+    console.log("Someone requested for the home page");
+    res.send("Hi there, welcome to my assignment");
 });
-// "/bye" -> "GOODBYE!"
-app.get("/bye", function(req, res){
-    console.log("someone requested for the bye");
-    res.send("GOODBYE!");
+
+app.get("/speak/:animal", function(req, res){
+    var sounds = {
+        pig: "oink",
+        cow: "hamba",
+        cat: "i hate you hooman",
+        chicken: "kuk kuru ku",
+        goldfish: "..."
+    }
+    var animal = req.params.animal.toLowerCase();
+    var sound = sounds[animal];
+    // if(animal == "cow"){
+    //     sound = "hamba"
+    // }
+    // else if(animal == "pig"){
+    //     sound = "oink";
+    // }
+    // else if(animal == "chicken"){
+    //     sound = "kuk kuru ku";
+    // }
+    console.log("Someone requested for " + animal + " speak page")
+    res.send("this " + animal + " speaks " + sound);
 });
-// "/cat" -> "MEOW!"
-app.get("/cat", function(req, res){
-    console.log("someone requested for the cat");
-    res.send("MEOW!");
+
+app.get("/repeat/:message/:times", function(req, res){
+    var message = req.params.message;
+    var times = Number(req.params.times);
+    var result = "";
+    for(i=0; i<times; i++){
+        result += message + " ";
+    }
+
+    console.log("Someone requested for repeat " + message + " for " + times + " times ");
+    res.send(result);
 });
-// Tell Express to listen for request (start server)
+
+app.get("*", function(req, res){
+    console.log("someone made a wrong request");
+    res.send("sorry, page not found ... what are you doing with your life");
+});
+
 app.listen(3000, function(){
-    console.log("Server has started!");  
+    console.log("Server is connected");
 });
