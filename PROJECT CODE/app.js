125352c2e8fc56c0c5f1599870e5338971d93323
Borshon96 <sajid.borshon@northsouth.edu>
2019-04-07 08:05:34 +0600

    updated app.js to introduce with ejs(embaded js)

--- PROJECT CODE/app.js
+++ PROJECT CODE/app.js
@@ -1,51 +1,37 @@
-var express = require("express");
+var express = require("express")
 var app = express();
 
-app.get("/", function(req, res){
-    console.log("Someone requested for the home page");
-    res.send("Hi there, welcome to my assignment");
+app.use(express.static("public"));
+
+app.get("/", function(req,res){
+    res.render("landing.ejs");
 });
 
-app.get("/speak/:animal", function(req, res){
-    var sounds = {
-        pig: "oink",
-        cow: "hamba",
-        cat: "i hate you hooman",
-        chicken: "kuk kuru ku",
-        goldfish: "..."
-    }
-    var animal = req.params.animal.toLowerCase();
-    var sound = sounds[animal];
-    // if(animal == "cow"){
-    //     sound = "hamba"
-    // }
-    // else if(animal == "pig"){
-    //     sound = "oink";
-    // }
-    // else if(animal == "chicken"){
-    //     sound = "kuk kuru ku";
-    // }
-    console.log("Someone requested for " + animal + " speak page")
-    res.send("this " + animal + " speaks " + sound);
+app.get("/playgrounds", function(req,res){
+    res.render("playgrounds.ejs");
 });
 
-app.get("/repeat/:message/:times", function(req, res){
-    var message = req.params.message;
-    var times = Number(req.params.times);
-    var result = "";
-    for(i=0; i<times; i++){
-        result += message + " ";
-    }
-
-    console.log("Someone requested for repeat " + message + " for " + times + " times ");
-    res.send(result);
+app.get("/contactus", function(req,res){
+    res.render("contact us.ejs");
 });
 
-app.get("*", function(req, res){
-    console.log("someone made a wrong request");
-    res.send("sorry, page not found ... what are you doing with your life");
+app.get("/about", function(req,res){
+    res.render("about.ejs");
 });
 
+app.get("/login", function(req,res){
+    res.render("login.ejs");
+});
+
+app.get("/signup", function(req,res){
+    res.render("sign up.ejs");
+});
+
+app.get("/*", function(req,res){
+    res.send("404 ERROR! NOT FOUND");
+});
+
+// Tell Express to listen for request (start server)
 app.listen(3000, function(){
-    console.log("Server is connected");
+    console.log("Server has started!");  
 });
